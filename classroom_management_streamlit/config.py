99d142a638ed85fe01755d69e16287383ea1126f
krutarth0505 <krutarth.g05@gmail.com>
2026-04-03 18:21:20 +0530

    st.secrets

--- classroom_management_streamlit/config.py
+++ classroom_management_streamlit/config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI") or os.getenv("MONGODB_ATLAS_URI") or "mongodb://localhost:27017"
-DB_NAME = os.getenv("DB_NAME", "classroom_management")
-MONGO_SERVER_SELECTION_TIMEOUT_MS = int(os.getenv("MONGO_SERVER_SELECTION_TIMEOUT_MS", "5000"))
+MONGO_URI = st.secrets("MONGO_URI") or st.secrets("MONGODB_ATLAS_URI") or "mongodb://localhost:27017"
+DB_NAME = st.secrets("DB_NAME") or st.secrets("DB_NAME", "classroom_management")
+MONGO_SERVER_SELECTION_TIMEOUT_MS = int(st.secrets("MONGO_SERVER_SELECTION_TIMEOUT_MS", "5000"))
 
 APP_TITLE = "Classroom Management System"
 DEFAULT_SUBJECT_NAME = "Classroom Subject"
